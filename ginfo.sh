d92449d62390c4482b8490598a00a795d06f1373
zhun <zhun@huoli.com>
2016-06-22 18:40:58 +0800

    basic command and read keybord input

---
+++ ginfo.sh
@@ -0,0 +1,9 @@
+clear
+echo 'hello';$user
+echo 'current date';date
+echo 'currnet user numbers'; who | wc -l
+echo 'calendar';cal
+
+echo 'your name, please:'
+read name
+echo "hello $name, lets be friend"
