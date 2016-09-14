c9b54ead86e7f8967873f5278f67265da1c536f5
zhun <zhun@huoli.com>
2016-09-14 12:48:39 +0800

    add script for checking branches not merged into release

---
+++ noMergedRelease.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+username=$1;
+echo "branches not merged into origin/release $username"
+
+getNoMergedBranch () {
+ for branch in `git branch -r --no-merged origin/release `; do echo -e `git show --format="%ai %ar by %an" $branch | head -n 1` \\t$branch;      done | sort -r
+} 
+
+if [ $username ]; then
+  getNoMergedBranch |grep $username
+else
+   getNoMergedBranch
+fi  
