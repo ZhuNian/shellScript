90dc7fd76290eb80abfaf6d992904131d91efe2a
zhun <zhun@huoli.com>
2016-09-14 13:11:07 +0800

    fix invalid origin refs

--- noMergedRelease.sh
+++ noMergedRelease.sh
@@ -3,6 +3,7 @@ username=$1;
 echo "branches not merged into origin/release $username"
 
 getNoMergedBranch () {
+ git fetch -p origin
  for branch in `git branch -r --no-merged origin/release `; do echo -e `git show --format="%ai %ar by %an" $branch | head -n 1` \\t$branch;      done | sort -r
 } 
 
