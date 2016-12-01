aea5c8e96590d14a3d12a6129ea4631498bb2f7f
zhun <zhun@huoli.com>
2016-12-01 19:49:48 +0800

    get project by name

--- createMR.sh
+++ createMR.sh
@@ -5,10 +5,13 @@ REDCOL='\e[31m'
 BLUECOL='\e[34m'
 ENDCOL='\e[0m'
 
-read -p "please input the project id" project
+read -p "please input the project name>> " projectName
+project=$(curl --header "PRIVATE-TOKEN: fwUBmDfesQGZQCxzvpUR" "http://gitlab.qtang.net/api/v3/projects/hangban%2F$projectName" -s | cut -d ',' -f1 | cut -d ':' -f2) 
 project=${project:-102}
 
 read -p "please input the source branch>> " sourceBranch
+currentBranch=$(git branch | grep \* | cut -d ' ' -f2);
+sourceBranch=${sourceBranch:-$currentBranch}
 
 read -p "please input the target branch>> " targetBranch
 targetBranch=${targetBranch:-test}
@@ -17,7 +20,8 @@ read -p "please input the title of your merge request>> " title
 title=${title:-test merge request}
 
 read -p "please input the desc of your merge reques>> " desc
-desc=${desc:-test merge request}
+lastCommit=$(git log -1 --pretty=%B)
+desc=${desc:-$lastCommit}
 
 read -p "please input the label of your request" label
 label=${label:-feature}
@@ -25,6 +29,8 @@ labels=();
 labels+=(${label})
 
 echo -e "creating merge request:
+project: ${REDCOL}$project${ENDCOL}
+projectName: ${BLUECOL}$projectName${ENDCOL}
 sourceBranch: ${BLUECOL}$sourceBranch${ENDCOL}
 targetBranch: ${BLUECOL}$targetBranch${ENDCOL}
 title: ${BLUECOL}$title${ENDCOL} 
