765b1511c5c762dc3dcdfa634fd4a443c2dd9e38
nian <zhun@133.cn>
2017-05-08 09:38:45 +0800

    refine

--- randomWallpaper.sh
+++ randomWallpaper.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-shopt -s nullglob
-
-cd ~/Pictures/wallpaper
-
-while true; do
-  files=()
-  for i in *.jpg *.png; do
-    [[ -f $i  ]] && files+=("$i")
-  done
-  range=${#files[@]}
-
-  ((range)) && feh --bg-scale "${files[RANDOM % range]}"
-
-done
+feh --bg-fill --randomize /home/nian/Pictures/wallpaper/* 
